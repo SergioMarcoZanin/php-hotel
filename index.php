df3f8bd75d888274eb6955b2e183ded8697273fd
SergioMarcoZanin <zaninsergiomarco@gmail.com>
2022-12-14 16:53:19 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Torino',
+            'description' => 'Hotel Torino Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 5.4
+        ],
+        [
+            'name' => 'Hotel Continental',
+            'description' => 'Hotel Continental Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Liguria',
+            'description' => 'Hotel Liguria Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 0.7
+        ],
+        [
+            'name' => 'Hotel Roma',
+            'description' => 'Hotel Roma Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 1.5
+        ],
+        [
+            'name' => 'Hotel Piccolo',
+            'description' => 'Hotel Piccolo Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 25
+        ],
+
+    ];
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PhP Hotel</title>
+</head>
+<body>
+    
+</body>
+</html>
